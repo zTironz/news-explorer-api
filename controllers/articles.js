01ee504eb1484ede0130ae31eb2dfc6f5ddaeb93
Tiron <ole_g92@bk.ru>
2020-08-05 00:15:50 +0300

    реализация контроллеров, роутов, аутентификации, авторизации, логирования

---
+++ controllers/articles.js
@@ -0,0 +1,49 @@
+const Article = require('../models/article');
+const BadRequest = require('../errors/bad-request');
+const NotFoundError = require('../errors/not-found-err');
+const Unauthorized = require('../errors/unauthorized');
+
+const getArticles = (req, res, next) => {
+  Article.find({ owner: req.user._id })
+    .then((articles) => res.send({ articles }))
+    .catch(next);
+};
+
+const createArticle = (req, res, next) => {
+  const {
+    keyword, title, text, date, source, link, image,
+  } = req.body;
+  const id = req.user._id;
+  Article.create({
+    keyword, title, text, date, source, link, image, owner: id,
+  })
+    .then((article) => res.send({ data: article }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        throw new BadRequest({ message: err.message });
+      }
+      return err;
+    })
+    .catch(next);
+};
+
+const deleteArticle = (req, res, next) => {
+  Article.findById(req.params.articleId)
+    .then((article) => {
+      if (!article) {
+        throw new NotFoundError('Нет статьи');
+      }
+      if (article.owner.toString() !== req.user._id) {
+        throw new Unauthorized('Недостаточно прав');
+      }
+      res.send({ data: article });
+      article.remove();
+    })
+    .catch(next);
+};
+
+module.exports = {
+  getArticles,
+  createArticle,
+  deleteArticle,
+};
---
+++ controllers/users.js
@@ -0,0 +1,62 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const NotFoundError = require('../errors/not-found-err');
+const UniqueError = require('../errors/unique-error');
+const BadRequest = require('../errors/bad-request');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+module.exports.getUser = (req, res, next) => {
+  User
+    .findById(req.user._id)
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Такого пользователя нет');
+      } else {
+        res.send({ user });
+      }
+    })
+    .catch(next);
+};
+
+module.exports.createUser = (req, res, next) => {
+  const {
+    name, email, password,
+  } = req.body;
+  const regExp = /(?=^.{8,}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*/;
+  if (!password) {
+    throw new BadRequest('Пароль не задан');
+  }
+  if (!regExp.test(password)) {
+    throw new BadRequest('Пароль должен быть не менее 8 символов и содержать цифры и латинские буквы');
+  }
+  bcrypt.hash(req.body.password, 10)
+    .then((hash) => User.create({
+      name, email, password: hash,
+    }))
+    .then((user) => res.send({
+      name: user.name, email: user.email,
+    }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new UniqueError(`Данные некорректны: ${err.message}`));
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  if (password) {
+    return User
+      .findUserByCredentials(email, password)
+      .then((userObj) => {
+        const token = jwt.sign({ _id: userObj._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
+        res.send({ token });
+      })
+      .catch(next);
+  }
+  throw new BadRequest('Необходимо ввести пароль');
+};
